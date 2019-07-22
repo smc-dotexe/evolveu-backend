6c2877758bc3af42cbee60ed0d7c1869ad1b6e4a
Shane McGuire <shanelgmcguire@gmail.com>
2019-07-22 11:56:39 -0600

    edited parts component

--- comp250/techlist/src/components/Parts.js
+++ comp250/techlist/src/components/Parts.js
@@ -25,23 +25,56 @@ class Parts extends React.Component {
     //     })
     // }
 
+    submitParts = e => {
+        let partDescription, partCost
+
+        partDescription = document.getElementById('partDescription').value
+        partCost = document.getElementById('partCost').value
+        
+    }
     render() {
         let partRows
         partRows = this.state.parts.map((part,key) => 
             <tr key={key}>
                 <td>{part.parts_id}</td>
+                <td>{part.job_id}</td>
                 <td>{part.description}</td>
                 <td>{part.cost}</td>
             </tr>
         )
 
+        let jobRows
+        jobRows = this.props.passJobs.map((job, key) => 
+            <option key={key} value={job.job_id}>{job.ro_number}</option>
+        )
+
         return (
             <div>
+                <div id ='addPartsDiv'>
+                    <form method='POST' onSubmit={this.submitParts}>
+                        <select id='selectJob'>
+                            <option value='select'>Select Job ID</option>
+                            {jobRows}
+                        </select><br />
+                        <input 
+                            id='partDescription' 
+                            type='text' 
+                            placeholder='Part Description'>
+                        </input><br />
+                        <input 
+                            id='partCost' 
+                            type='number' 
+                            placeholder='Price'>
+                        </input><br />
+                        <input type='submit' value='Submit'></input>
+                    </form>
+                </div>
                 <div id='partsTableDiv'>
                     <table id='partsTable'>
                         <tbody>
                             <tr>
                                 <td>Part ID</td>
+                                <td>Job ID</td>
                                 <td>Description</td>
                                 <td>Cost</td>
                             </tr>
